e37d76216061337d4ebd8d27493df275eb9aed3d
Alvaro Rodriguez Gonzalez <alvamige@gmail.com>
2023-05-19 20:59:50 +0200

    Parts test

--- src/tests/jest.config.ts
+++ src/tests/jest.config.ts
@@ -1,11 +1,11 @@
 export default {
-	rootDir: "./../..",
-	transform: {
-		"^.+\\.tsx?$": "ts-jest",
-	},
-	collectCoverage: true,
-	collectCoverageFrom: ["src/controllers/*controller.ts"],
-	globalSetup: "./src/tests/utils/startTest.ts",
-	globalTeardown: "./src/tests/utils/endTest.ts",
-	testTimeout: 20000,
+  rootDir: "./../..",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ["src/controllers/*controller.ts", "src/repositories/*repository.ts", "src/models/*model.ts"],
+  globalSetup: "./src/tests/utils/startTest.ts",
+  globalTeardown: "./src/tests/utils/endTest.ts",
+  testTimeout: 20000,
 };
